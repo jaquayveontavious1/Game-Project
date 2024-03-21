6838042e1e22452d5596277fa0a5de5f6eede9d8
jaquayveontavious1 <Kanyingitiffany@gmail.com>
2024-03-21 22:21:10 +0300

    connected CSS with JS

--- project.js
+++ project.js
@@ -4,6 +4,10 @@ const choices = ['rock','paper','scissors'];
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 //created a function hence when a button is clicked(onclick) this function is executed
 function playGame (playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() *3)];
@@ -37,9 +41,13 @@ function playGame (playerChoice) {
     switch (result) {
         case "YOU WIN" :
             resultDisplay.classList.add("greenText");
+            playerScore ++;
+            playerScoreDisplay.textContent = playerScore;
             break;
         case "YOU LOSE" :
             resultDisplay.classList.add("redText");
+            computerScore ++;
+            computerScoreDisplay.textContent = computerScore;
             break;
     }
     console.log(result);
